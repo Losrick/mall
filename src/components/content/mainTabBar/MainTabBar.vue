22585e9729cbeeb136a4964768e5f42097eb28a8
Losrick <674767151@qq.com>
2021-06-08 11:08:41 +0800

    删除多余的代码

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -1,27 +1,11 @@
 <template>
   <tab-bar>
     <tab-bar-item path="/home" activeColor="#f54848">
-      <!-- <template v-slot:item-icon
-        ><img src="@/assets/img/tabbar/home.svg" alt=""
-      /></template>
-      <template v-slot:item-icon-active
-        ><img src="@/assets/img/tabbar/home_active.svg" alt=""
-      /></template>
-      <template v-slot:item-text>
-        <div>首页</div>
-      </template> -->
       <img slot="item-icon" src="@/assets/img/tabbar/home.svg" alt="" />
       <img slot="item-icon-active" src="@/assets/img/tabbar/home_active.svg" />
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="#f54848">
-      <!-- <template v-slot:item-icon
-        ><img src="@/assets/img/tabbar/category.svg" alt="" />
-      </template>
-      <template v-slot:item-icon-active>
-        <img src="@/assets/img/tabbar/category_active.svg" alt=""
-      /></template>
-      <template v-slot:item-text><div>分类</div> </template> -->
       <img slot="item-icon" src="@/assets/img/tabbar/category.svg" alt="" />
       <img
         slot="item-icon-active"
@@ -31,13 +15,6 @@
       <div slot="item-text">分类</div>
     </tab-bar-item>
     <tab-bar-item path="/cart" activeColor="#f54848">
-      <!-- <template v-slot:item-icon
-        ><img src="@/assets/img/tabbar/cart.svg" alt="" />
-      </template>
-      <template v-slot:item-icon-active
-        ><img src="@/assets/img/tabbar/cart_active.svg" alt="" />
-      </template>
-      <template v-slot:item-text> <div>购物车</div></template> -->
       <img slot="item-icon" src="@/assets/img/tabbar/cart.svg" alt="" />
       <img
         slot="item-icon-active"
@@ -54,13 +31,6 @@
         <img src="@/assets/img/tabbar/profile_active.svg" alt=""
       /></template>
       <template v-slot:item-text> <div>我的</div></template>
-      <!-- <img slot="item-icon" src="@/assets/img/tabbar/profile.svg" alt="" />
-      <img
-        slot="item-icon-active"
-        src="@/assets/img/tabbar/profile_active.svg"
-        alt=""
-      /> -->
-      <!-- <div slot="item-text">我的</div> -->
     </tab-bar-item>
   </tab-bar>
 </template>
